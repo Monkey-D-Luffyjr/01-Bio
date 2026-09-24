584239f984092407524fa66af90167c176c6b9a8
Mokey-D-Luffyjr <monkeydluffyjr12@gmail.com>
2026-09-24 18:18:56 +0100

    Day2/3: I'm currently working on a task that requires scripting

---
+++ script.js
@@ -0,0 +1,9 @@
+//This is a function that displays an alert to the user on the web page.
+function showMessage(){
+    alert("Yoooo, I'm finally running via an extended js file!!!!");
+}
+
+
+function randomMessage(){
+    var quotes = ['My name\'s Moses', 'I loveeeee playing Local Warfare Re-portable with the bros', 'I also love watching anime and practicing Japaneseeee.']
+}
